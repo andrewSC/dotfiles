a67fc046e6bc68ca432c04babbf75679a46c7e72
Andrew Crerar <crerar@adobe.com>
2015-02-17 10:16:29 -0500

    Aliases: Adding dev23 alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -152,10 +152,13 @@ alias updatedb='/usr/libexec/locate.updatedb'
 alias caladanup='VBoxManage startvm Arch\ Linux --type headless'
 
 # fswatch
-alias watch-ccn="cd ~/Dev/ccn && watcher ~/Dev/ccn/ sbelsky@dev23.be.lan:/var/www/vhosts/ccn/sandbox/"
-alias watch-net="cd ~/Dev/be.net && watcher ~/Dev/be.net/ sbelsky@dev23.be.lan:/var/www/vhosts/network/sandbox/"
+alias watch-ccn='cd ~/Dev/ccn && watcher ~/Dev/ccn/ sbelsky@dev23.be.lan:/var/www/vhosts/ccn/sandbox/'
+alias watch-net='cd ~/Dev/be.net && watcher ~/Dev/be.net/ sbelsky@dev23.be.lan:/var/www/vhosts/network/sandbox/'
 
 # Ctags indexer
 alias tagall='sh /Users/crerar/.bash_scripts/tag_indexer_net.sh && sh /Users/crerar/.bash_scripts/tag_indexer_ccn.sh && sh /Users/crerar/.bash_scripts/tag_indexer_core.sh'
+
+# Vagrant
+alias dev23="cd ~/bodega/dev23 && vagrant up && ssh -p 2222 andrew@127.0.0.1"
 
 
