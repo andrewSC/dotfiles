3589429acf8f1b55af4a1f3a88ea32de87f4a5fa
Andrew Crerar <ascrerar@ncsu.edu>
2017-12-12 14:18:35 -0500

    Adding hammerspoon window sizing config to repo

---
+++ hammerspoon/init.lua
@@ -0,0 +1,100 @@
+-- Global options
+hs.window.animationDuration = 0
+
+-- Top Left
+hs.hotkey.bind({"alt", "cmd"}, "U", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Top Right
+hs.hotkey.bind({"alt", "cmd"}, "O", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Bottom Left
+hs.hotkey.bind({"alt", "cmd"}, "M", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Bottom Right
+hs.hotkey.bind({"alt", "cmd"}, ".", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y + (max.h / 2)
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Left 1/2 Screen
+hs.hotkey.bind({"alt", "cmd"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Right 1/2 Screen
+hs.hotkey.bind({"alt", "cmd"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Left 2/3 Screen
+hs.hotkey.bind({"ctrl", "cmd"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = (max.w / 3) * 2
+  f.h = max.h
+  win:setFrame(f)
+end)
