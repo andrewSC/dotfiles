5a43f798de5b14549d91e0779cb7a7011499763a
Andrew Crerar <ascrerar@ncsu.edu>
2017-12-13 08:59:26 -0500

    Removing signing from gitconfig, adding fullscreen to hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,20 @@
 -- Global options
 hs.window.animationDuration = 0
 
+-- Fullscreen
+hs.hotkey.bind({"alt", "cmd"}, "X", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
 -- Top Left
 hs.hotkey.bind({"alt", "cmd"}, "U", function()
   local win = hs.window.focusedWindow()
